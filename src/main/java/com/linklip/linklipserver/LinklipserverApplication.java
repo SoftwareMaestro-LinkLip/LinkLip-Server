80b3927f187e2db9a78e73486fdc1bcb0ea2c187
youngkwon02 <youngkwon02@naver.com>
2022-07-12 14:10:48 +0900

    [CHORE] spring boot project init

---
+++ src/main/java/com/linklip/linklipserver/LinklipserverApplication.java
@@ -0,0 +1,13 @@
+package com.linklip.linklipserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinklipserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinklipserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/linklip/linklipserver/LinklipserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linklip.linklipserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinklipserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
