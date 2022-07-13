84ebd9ca95b1a9da745131f09ec27fbadad75244
junwoo <top7578@naver.com>
2022-07-14 01:46:18 +0900

    feat: [GYJB-55] Swagger 설정

--- src/main/java/com/linklip/linklipserver/config/SwaggerConfig.java
+++ src/main/java/com/linklip/linklipserver/config/SwaggerConfig.java
@@ -10,6 +10,8 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+
+// 접속 방법 : http://localhost:8080/swagger-ui.html
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
--- src/main/java/com/linklip/linklipserver/controller/SwaggerTestController.java
+++ src/main/java/com/linklip/linklipserver/controller/SwaggerTestController.java
@@ -8,13 +8,13 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.web.bind.annotation.*;
 
-@Api(value = "SwaggerTestController")
+@Api(value = "SwaggerTestController")   // 클래스를 Swagger 리소스 대상으로 표시
 @RestController
 @RequestMapping("/api")
 public class SwaggerTestController {
 
-    @ApiOperation(value = "test", notes = "테스트입니다.")
-    @ApiResponses({
+    @ApiOperation(value = "test", notes = "테스트입니다.")    // 요청 URL에 매핑된 API 에 대한 설명
+    @ApiResponses({     // 응답에 대한 설명
             @ApiResponse(code = 200, message = "ok"),
             @ApiResponse(code = 500, message = "server error"),
             @ApiResponse(code = 404, message = "not found")
