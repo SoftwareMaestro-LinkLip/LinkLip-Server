3d12a76f3a3fbee4aafce0b5d8302534b4e002eb
youngkwon02 <youngkwon02@naver.com>
2022-07-18 16:55:57 +0900

    [GYJB-51] Content entity 추가

--- src/main/java/com/linklip/linklipserver/domain/Content.java
+++ src/main/java/com/linklip/linklipserver/domain/Content.java
@@ -1,2 +1,24 @@
-package com.linklip.linklipserver.domain;public class Content {
+package com.linklip.linklipserver.domain;
+
+
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class Content {
+
+    @Id @GeneratedValue
+    @Column(name = "content_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String linkUrl;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+
 }
