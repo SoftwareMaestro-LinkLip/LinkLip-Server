9394ba6b23ca62ed9dbf53d58eb44fb60ddff9dd
youngkwon02 <youngkwon02@naver.com>
2022-07-20 03:26:09 +0900

    [GYJB-52] Success, Error Response enum으로 상수화

--- src/main/java/com/linklip/linklipserver/constant/ErrorResponse.java
+++ src/main/java/com/linklip/linklipserver/constant/ErrorResponse.java
@@ -1,2 +1,25 @@
-package com.linklip.linklipserver.constant;public enum ErrorResponse {
+package com.linklip.linklipserver.constant;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+public enum ErrorResponse {
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "ERR01", "잘못된 요청입니다"),
+    NOT_FOUND(HttpStatus.NOT_FOUND, "ERR01", "요청 경로 오류"),
+    INTERNAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "ERR02", "서버 내부 오류");
+
+    @Getter
+    private int status;
+
+    @Getter
+    private final String code;
+
+    @Getter
+    private final String message;
+
+    private ErrorResponse(HttpStatus status, String code, String message) {
+        this.status = status.value();
+        this.code = code;
+        this.message = message;
+    }
 }
--- src/main/java/com/linklip/linklipserver/constant/SuccessResponse.java
+++ src/main/java/com/linklip/linklipserver/constant/SuccessResponse.java
@@ -1,5 +1,23 @@
 package com.linklip.linklipserver.constant;
 
-public enum SuccessCode {
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
+public enum SuccessResponse {
+    SAVE_LINK_SUCCESS(HttpStatus.OK, "OK1", "링크 저장 완료");
+
+    @Getter
+    private int status;
+
+    @Getter
+    private final String code;
+
+    @Getter
+    private final String message;
+
+    private SuccessResponse(HttpStatus status, String code, String message) {
+        this.status = status.value();
+        this.code = code;
+        this.message = message;
+    }
 }
