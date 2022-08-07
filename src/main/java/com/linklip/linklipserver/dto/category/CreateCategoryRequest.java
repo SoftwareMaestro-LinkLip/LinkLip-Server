180859870ea5c8b2593475743dd466ab6a63b761
youngkwon02 <youngkwon02@naver.com>
2022-08-07 16:50:22 +0900

    [GYJB-105] CreateCategoryRequest에 NoArgsConstructor 추가

--- src/main/java/com/linklip/linklipserver/dto/category/CreateCategoryRequest.java
+++ src/main/java/com/linklip/linklipserver/dto/category/CreateCategoryRequest.java
@@ -3,8 +3,10 @@ package com.linklip.linklipserver.dto.category;
 import com.linklip.linklipserver.domain.Category;
 import javax.validation.constraints.NotEmpty;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class CreateCategoryRequest {
 
     @NotEmpty private String name;
