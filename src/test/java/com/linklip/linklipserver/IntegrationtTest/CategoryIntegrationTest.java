d459b886ba8594ebc937dd62bd690abf70a900c7
junwoo <top7578@naver.com>
2022-08-08 20:03:43 +0900

    [GYJB-101] 카테고리 수정 API 통합테스트 작성

--- src/test/java/com/linklip/linklipserver/IntegrationtTest/CategoryIntegrationTest.java
+++ src/test/java/com/linklip/linklipserver/IntegrationtTest/CategoryIntegrationTest.java
@@ -1,11 +1,13 @@
 package com.linklip.linklipserver.IntegrationtTest;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.linklip.linklipserver.domain.Category;
 import com.linklip.linklipserver.dto.category.CreateCategoryRequest;
+import com.linklip.linklipserver.dto.category.UpdateCategoryRequest;
 import com.linklip.linklipserver.repository.CategoryRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -15,6 +17,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
@@ -93,6 +96,81 @@ public class CategoryIntegrationTest {
         public void findCategory() throws Exception {
             mockMvc.perform(get("/category/v1").contentType(MediaType.APPLICATION_JSON))
                     .andExpect(status().isOk());
+        }
+    }
+
+    @Nested
+    @DisplayName("카테고리 수정 통합테스트")
+    class updateCategoryIntegrationTest {
+        @Test
+        @DisplayName("일반적인 카테고리명 수정")
+        public void updateCategory() throws Exception {
+
+            // given
+            Category category = Category.builder().name("운동").build();
+            Category entity = categoryRepository.save(category);
+            long categoryId = entity.getId();
+
+            // when
+            UpdateCategoryRequest updateCategoryRequest = new UpdateCategoryRequest();
+            updateCategoryRequest.setName("스포츠");
+
+            ResultActions actions =
+                    mockMvc.perform(
+                            patch("/category/v1/{id}", categoryId)
+                                    .contentType(MediaType.APPLICATION_JSON)
+                                    .content(asJsonString(updateCategoryRequest)));
+
+            // then
+            actions.andExpect(status().isOk()).andDo(print());
+        }
+
+        @Test
+        @DisplayName("이미 존재하는 카테고리명으로 수정")
+        public void updateCategoryByPresentName() throws Exception {
+
+            // given
+            Category category1 = Category.builder().name("운동").build();
+            Category entity = categoryRepository.save(category1);
+            Category category2 = Category.builder().name("스포츠").build();
+            categoryRepository.save(category2);
+            long categoryId = entity.getId();
+
+            // when
+            UpdateCategoryRequest updateCategoryRequest = new UpdateCategoryRequest();
+            updateCategoryRequest.setName("스포츠");
+
+            ResultActions actions =
+                    mockMvc.perform(
+                            patch("/category/v1/{id}", categoryId)
+                                    .contentType(MediaType.APPLICATION_JSON)
+                                    .content(asJsonString(updateCategoryRequest)));
+
+            // then
+            actions.andExpect(status().isOk());
+        }
+
+        @Test
+        @DisplayName("빈값으로 카테고리명 수정")
+        public void updateCategoryByNullName() throws Exception {
+
+            // given
+            Category category = Category.builder().name("운동").build();
+            Category entity = categoryRepository.save(category);
+            long categoryId = entity.getId();
+
+            // when
+            UpdateCategoryRequest updateCategoryRequest = new UpdateCategoryRequest();
+            updateCategoryRequest.setName("");
+
+            ResultActions actions =
+                    mockMvc.perform(
+                            patch("/category/v1/{id}", categoryId)
+                                    .contentType(MediaType.APPLICATION_JSON)
+                                    .content(asJsonString(updateCategoryRequest)));
+
+            // then
+            actions.andExpect(status().isBadRequest());
         }
     }
 
