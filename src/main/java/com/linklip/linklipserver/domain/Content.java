39afd15af6776ad2ece26a902cf0e4446e36acfc
youngkwon02 <youngkwon02@naver.com>
2022-08-09 15:46:41 +0900

    [GYJB-109] ContentController에서 ResponseEntity를 Return하도록 수정

--- src/main/java/com/linklip/linklipserver/domain/Content.java
+++ src/main/java/com/linklip/linklipserver/domain/Content.java
@@ -1,12 +1,10 @@
 package com.linklip.linklipserver.domain;
 
-import java.time.LocalDateTime;
 import javax.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
 
 @Entity
 @Getter
