eb7e0c5a3730970c9a7d1151bf14f4a8a03aad46
junwoo <top7578@naver.com>
2022-08-10 02:11:48 +0900

    [GYJB-108] 카테고리 검색 메서드 작성

--- src/main/java/com/linklip/linklipserver/repository/CategoryRepository.java
+++ src/main/java/com/linklip/linklipserver/repository/CategoryRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     List<Category> findAllByOrderByName();
+
+    List<Category> findAllByNameContains(String name);
 }
