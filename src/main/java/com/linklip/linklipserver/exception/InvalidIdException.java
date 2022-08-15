de493925bbb5b0a4edd68899c5fff4f5d95ed4c6
youngkwon02 <youngkwon02@naver.com>
2022-08-15 16:52:40 +0900

    [GYJB-138] InvalidIdException 생성

---
+++ src/main/java/com/linklip/linklipserver/exception/InvalidIdException.java
@@ -0,0 +1,8 @@
+package com.linklip.linklipserver.exception;
+
+public class InvalidIdException extends RuntimeException{
+
+    public InvalidIdException(String message) {
+        super(message);
+    }
+}
