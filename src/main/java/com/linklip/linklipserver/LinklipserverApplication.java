03af3cc0ac5bcdece5af4f9f817b19e545cf2e57
youngkwon02 <youngkwon02@naver.com>
2022-08-22 16:28:26 +0900

    [GYJB-157] google java format 적용

--- src/main/java/com/linklip/linklipserver/LinklipserverApplication.java
+++ src/main/java/com/linklip/linklipserver/LinklipserverApplication.java
@@ -2,9 +2,6 @@ package com.linklip.linklipserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class LinklipserverApplication {
