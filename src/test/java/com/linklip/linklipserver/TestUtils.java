2f44547abde563d9abb15aab3b295bb17956d9d2
junwoo <top7578@naver.com>
2022-08-30 20:53:09 +0900

    [GYJB-162] test 수정

--- src/test/java/com/linklip/linklipserver/TestUtils.java
+++ src/test/java/com/linklip/linklipserver/TestUtils.java
@@ -3,6 +3,7 @@ package com.linklip.linklipserver;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.linklip.linklipserver.domain.Category;
 import com.linklip.linklipserver.domain.Content;
+import com.linklip.linklipserver.domain.Link;
 import com.linklip.linklipserver.repository.CategoryRepository;
 import com.linklip.linklipserver.repository.ContentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,10 +15,10 @@ public class TestUtils {
     @Autowired private ContentRepository contentRepository;
     @Autowired private CategoryRepository categoryRepository;
 
-    public Content saveContent(String url, String title, String text, Category category) {
+    public Content saveLink(String url, String title, String text, Category category) {
 
         Content content =
-                Content.builder().linkUrl(url).title(title).text(text).category(category).build();
+                Link.builder().linkUrl(url).title(title).text(text).category(category).build();
         contentRepository.save(content);
 
         return content;
