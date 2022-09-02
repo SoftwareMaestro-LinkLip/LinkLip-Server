463bb081093d1da041a462a40da57f12b1f59e1f
youngkwon02 <youngkwon02@naver.com>
2022-09-02 18:01:00 +0900

    [TEST] 메모 저장 API 단위테스트 작성

--- src/main/java/com/linklip/linklipserver/domain/Link.java
+++ src/main/java/com/linklip/linklipserver/domain/Link.java
@@ -1,6 +1,5 @@
 package com.linklip.linklipserver.domain;
 
-import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.Lob;
