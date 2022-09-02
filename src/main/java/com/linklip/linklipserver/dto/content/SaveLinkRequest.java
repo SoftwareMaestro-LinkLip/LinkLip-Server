ab1f088d9e29eab38d4a2788d649e3aa0ff552d9
youngkwon02 <youngkwon02@naver.com>
2022-09-03 02:42:02 +0900

    [CHORE] SaveLinkRequest 사용하지 않는 코드 삭제

--- src/main/java/com/linklip/linklipserver/dto/content/SaveLinkRequest.java
+++ src/main/java/com/linklip/linklipserver/dto/content/SaveLinkRequest.java
@@ -1,14 +1,11 @@
 package com.linklip.linklipserver.dto.content;
 
-import com.linklip.linklipserver.service.CategoryService;
 import io.swagger.annotations.ApiModelProperty;
 import javax.validation.constraints.NotEmpty;
 import lombok.Data;
 
 @Data
 public class SaveLinkRequest {
-
-    private CategoryService categoryService;
 
     @ApiModelProperty(required = true)
     @NotEmpty
