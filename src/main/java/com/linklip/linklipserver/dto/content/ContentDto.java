54491f616161089352a11e1a2360a38e2b157946
youngkwon02 <youngkwon02@naver.com>
2022-09-03 15:09:08 +0900

    [REFACTOR] ContentDTO Type별로 분리

---
+++ src/main/java/com/linklip/linklipserver/dto/content/ContentDto.java
@@ -0,0 +1,11 @@
+package com.linklip.linklipserver.dto.content;
+
+import com.linklip.linklipserver.dto.category.CategoryDto;
+
+public class ContentDto {
+
+    public String type;
+    public Long id;
+    public CategoryDto category;
+
+}
---
+++ src/main/java/com/linklip/linklipserver/dto/content/NoteDto.java
@@ -0,0 +1,19 @@
+package com.linklip.linklipserver.dto.content;
+
+import com.linklip.linklipserver.domain.Note;
+import com.linklip.linklipserver.dto.category.CategoryDto;
+import lombok.Getter;
+
+@Getter
+public class NoteDto extends ContentDto {
+    private final String text;
+
+    public NoteDto(Note content) {
+        this.id = content.getId();
+        this.type = content.getType();
+        this.text = content.getText();
+        if (content.getCategory() != null) {
+            this.category = new CategoryDto(content.getCategory());
+        }
+    }
+}
