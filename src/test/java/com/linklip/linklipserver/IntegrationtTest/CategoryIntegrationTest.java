43545f73efc6049cc89b4c548f68dff3057ab8cc
junwoo <top7578@naver.com>
2022-09-12 21:24:08 +0900

    [FEAT] GYJB-193 인증된 상태로 테스트 진행하도록 설정

--- src/test/java/com/linklip/linklipserver/IntegrationtTest/CategoryIntegrationTest.java
+++ src/test/java/com/linklip/linklipserver/IntegrationtTest/CategoryIntegrationTest.java
@@ -21,6 +21,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
@@ -28,6 +29,7 @@ import org.springframework.transaction.annotation.Transactional;
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
+@WithMockUser
 public class CategoryIntegrationTest {
 
     // @FIXME: 해당 변수에 빨간줄 들어오는데 정확한 이유 파악 필요
