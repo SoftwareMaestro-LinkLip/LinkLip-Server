28081a1e4d4144d6079c5a7eb7a94216213a0f97
junwoo <top7578@naver.com>
2022-09-12 21:04:50 +0900

    [FEAT] GYJB-193 security config 설정

---
+++ src/main/java/com/linklip/linklipserver/config/AuthenticationConfig.java
@@ -0,0 +1,52 @@
+package com.linklip.linklipserver.config;
+
+import com.linklip.linklipserver.config.filter.JwtTokenFilter;
+import com.linklip.linklipserver.config.oauth.OAuth2SuccessHandler;
+import com.linklip.linklipserver.config.oauth.OAuth2UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록
+public class AuthenticationConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${jwt.secret-key}")
+    private String key;
+
+    @Value("${jwt.token-expired-time-ms}")
+    private Long expiredTime;
+
+    private final OAuth2UserService oauth2UserService;
+    private final OAuth2SuccessHandler oauth2SuccessHandler;
+    private final JwtTokenFilter jwtTokenFilter;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf()
+                .disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/content/**", "/category/**")
+                .authenticated()
+                .and()
+                .oauth2Login() // OAuth2 로그인 설정 시작점
+                .userInfoEndpoint() // OAuth2 로그인 성공 이후 사용자 정보를 가져올 때 설정 담당
+                .userService(
+                        oauth2UserService) // OAuth2 로그인 성공 시, 후작업을 진행할 UserService 인터페이스 구현체 등록
+                .and()
+                .successHandler(oauth2SuccessHandler)
+                .and()
+                .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling()
+                .authenticationEntryPoint(new CustomAuthenticationEntryPoint());
+    }
+}
