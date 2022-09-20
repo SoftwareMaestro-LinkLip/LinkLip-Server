ec619cb5f37c45a4c5ba3e14b9ab04214ee69124
junwoo <top7578@naver.com>
2022-09-20 14:49:44 +0900

    [FEAT] GYJB-204 RefreshToken 엔티티 생성

---
+++ src/main/java/com/linklip/linklipserver/domain/RefreshToken.java
@@ -0,0 +1,38 @@
+package com.linklip.linklipserver.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "token_id")
+    private Long id;
+
+    private String token;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Builder
+    public RefreshToken(Long id, String token, User user) {
+        this.id = id;
+        this.token = token;
+        this.user = user;
+    }
+}
