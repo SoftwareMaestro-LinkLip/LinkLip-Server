02e848101a5f37829a856a4fa117192527c10e00
junwoo <top7578@naver.com>
2022-10-03 01:05:49 +0900

    [FEAT] GYJB-205 사진 저장 API 구현

---
+++ src/main/java/com/linklip/linklipserver/service/S3Upload.java
@@ -0,0 +1,31 @@
+package com.linklip.linklipserver.service;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import java.io.IOException;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@RequiredArgsConstructor
+@Service
+public class S3Upload {
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    private final AmazonS3 amazonS3;
+
+    public String upload(MultipartFile multipartFile) throws IOException {
+        String s3FileName = UUID.randomUUID() + "-" + multipartFile.getOriginalFilename();
+
+        ObjectMetadata objectMetadate = new ObjectMetadata();
+        objectMetadate.setContentLength(multipartFile.getInputStream().available());
+
+        amazonS3.putObject(bucket, s3FileName, multipartFile.getInputStream(), objectMetadate);
+
+        return amazonS3.getUrl(bucket, s3FileName).toString();
+    }
+}
